d139003d8946c535935568e4e525b80929d18dfb
Keith Welch <keith@paralogiktech.com>
2020-06-29 21:56:27 -0400

    Updated install, better package detection and gpu_mem set

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 DD_HOME=~
 DD_VERSION="0.1"
-DD_NEEDS="php php-xml scrot unclutter "
 DD_CONFIG_DIR="$DD_HOME/.config/ddojo"
 DD_CONFIG="$DD_HOME/.config/ddojo/ddojo.conf"
 DD_INSTALLED_VERSION=""
@@ -17,6 +16,28 @@ else
 		echo "First time intalling making $DD_CONFIG_DIR and $DD_CONFIG"
 	fi
 fi
+function check_needs() {
+	DD_INSTALL_PKGS=""
+	for need in $1; do
+		NEED_TEST=`/usr/bin/dpkg -l $need 2>/dev/null | /bin/grep ^ii | /usr/bin/wc -l`
+		if [ "$NEED_TEST" != "1" ]; then
+			#echo "$need is not installed, will attempt to install"
+			if [ -z "$DD_INSTALL_PKGS" ]; then
+				DD_INSTALL_PKGS="$need"
+			else
+				DD_INSTALL_PKGS+=" $need"
+			fi
+		fi
+	done
+	echo $DD_INSTALL_PKGS
+}
+function set_gpu_mem() {
+	DD_CHK_MEM=`/bin/grep ^gpu_mem /boot/config.txt 2>/dev/null | /usr/bin/wc -l`
+	if [ "$DD_CHK_MEM" != "1" ]; then
+		echo "Setting gpu_mem=128 in /boot/config.txt"
+		echo "gpu_mem=128" | sudo /usr/bin/tee -a /boot/config.txt > /dev/null
+	fi
+}
 # TODO Need to do real version comparison
 # 1.10 > 1.9 or 1.1.3 < 1.0.2
 # For now if versions are different we'll overwrite
@@ -32,48 +53,24 @@ if [ ! -r "/etc/os-release" ]; then
 	exit
 fi
 source /etc/os-release
-DD_CHECK="wget chromium-browser php xdotool"
+DD_NEEDS="php-cli php-xml php-json php-tokenizer scrot unclutter wget xdotool chromium-browser"
 DD_CHROMIUM="/usr/bin/chromium-browser"
 DD_KILL_CHROMIUM_GREP="chromium-browser"
 DD_PKG_GET="/usr/bin/sudo apt-get install -y"
-if [ "$ID" == "arch" ]; then
-	DD_CHECK="wget chromium php xdotool"
-	DD_CHROMIUM="/usr/bin/chromium"
-	DD_KILL_CHROMIUM_GREP="chromium"
-	DD_PKG_GET="/usr/bin/sudo pacman --needed -S"
-	#$DD_PKG_GET php-xml
-elif [ "$ID" == "debian" ]; then
-	DD_CHECK="wget chromium php xdotool"
+if [ "$ID" == "debian" ]; then
+	DD_NEEDS="php-cli php-xml php-json php-tokenizer scrot unclutter wget xdotool chromium"
 	DD_CHROMIUM="/usr/bin/chromium"
 	DD_KILL_CHROMIUM_GREP="chromium"
 	#$DD_PKG_GET php-xml
+else
+	echo "Unsupported operating system"
+	exit
 fi
-for need in $DD_CHECK; do
-	NEED_TEST=`which $need`
-	if [ "$NEED_TEST" != "/usr/bin/$need" ]; then
-		echo "$need is not installed, will attempt to install"
-		if [ -z "$DD_NEEDS" ]; then
-			DD_NEEDS="$need"
-		else
-			DD_NEEDS+=" $need"
-		fi
-	fi
-done
-if [ -n "$DD_NEEDS" ]; then
-	echo "We need to install the following packages: $DD_NEEDS"
-	$DD_PKG_GET $DD_NEEDS
-	DD_NEEDS2=""
-	for need in $DD_CHECK; do
-		NEED_TEST=`which $need`
-		if [ "$NEED_TEST" != "/usr/bin/$need" ]; then
-			echo "$need failed to install"
-			if [ -z "$DD_NEEDS2" ]; then
-				DD_NEEDS2="$need"
-			else
-				DD_NEEDS2+=" $need"
-			fi
-		fi
-	done
+DD_INSTALL_PKGS=$( check_needs "$DD_NEEDS" )
+if [ -n "$DD_INSTALL_PKGS" ]; then
+	echo "We need to install the following packages: $DD_INSTALL_PKGS"
+	$DD_PKG_GET $DD_INSTALL_PKGS
+	DD_NEEDS2=$ ( check_needs "$DD_NEEDS2" )
 	if [ -n "$DD_NEEDS2" ]; then
 		echo "ERROR: Failed to install the following packages: $DD_NEEDS2, unable to continue"
 		exit
@@ -97,10 +94,11 @@ if [ "$DD_INSTALL" == "yes" ]; then
 	fi
 	git clone $DD_GIT_URL $DD_LOCAL_DIR
 	if [ "$DD_FIRST_INSTALL" == "yes" ]; then
-		echo "DD_INSTALLED_VERSION=\"$DD_VERSION\"" | tee --append $DD_CONFIG
+		echo "DD_INSTALLED_VERSION=\"$DD_VERSION\"" >> $DD_CONFIG
 	fi
 	sed -i "s/^\(DD_INSTALLED_VERSION\s*=\s*\).*\$/\1$DD_VERSION/" $DD_CONFIG
 	echo "Writing DD_INSTALLED_VERSION=$DD_VERSION to $DD_CONFIG"
+	set_gpu_mem
 fi
 if [ ! -x $DD_HOME/ddojo_local/bin/console ]; then
 	echo "ERROR: Unable to find local client files, unable to continue"
